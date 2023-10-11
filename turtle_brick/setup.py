c12f0a5efb660acbf1f6f5f2da589ffd2a24b1a5
dkoh555 <damienkoh2025@u.northwestern.edu>
2023-10-10 21:01:49 -0500

    got the robot_state_publisher working, and a super simple URDF loaded into rviz2

--- turtle_brick/setup.py
+++ turtle_brick/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'turtle_brick'
@@ -9,7 +11,9 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml', 'launch/show_turtle.launch.xml']),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name), glob('urdf/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,

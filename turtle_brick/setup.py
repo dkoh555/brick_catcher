bc649aa23c804719fda449934ac331cea23cea57
dkoh555 <damienkoh2025@u.northwestern.edu>
2023-10-10 20:38:57 -0500

    everything is all over the place, need to figure out how to set up the robot_state_publisher

--- turtle_brick/setup.py
+++ turtle_brick/setup.py
@@ -9,7 +9,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['package.xml', 'launch/show_turtle.launch.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'test_node = turtle_brick.test_node:main'
+            'test_node = turtle_brick.test_node:main',
+            'in_out = turtle_brick.in_out:in_out_entry'
         ],
     },
 )

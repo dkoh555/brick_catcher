d1038f00bd3a5e2b37044d169b8fb7d0fb8f5aa0
dkoh555 <damienkoh2025@u.northwestern.edu>
2023-10-11 04:33:49 -0500

    touched up launch file to include rviz, but still can't figure out how to combine the yaml and xacro files together

--- turtle_brick/setup.py
+++ turtle_brick/setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
         (os.path.join('share', package_name), glob('urdf/*')),
+        ('share/' + package_name + '/config/', ['config/turtle.yaml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,

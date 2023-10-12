c174f83b366796d7cde387708f503650ba4915fc
dkoh555 <damienkoh2025@u.northwestern.edu>
2023-10-11 20:47:35 -0500

    got a basic demo of the turtle_robot working with turtlesim and turtle_teleop!

--- turtle_brick/turtle_brick/turtle_robot.py
+++ turtle_brick/turtle_brick/turtle_robot.py
@@ -1,77 +1,176 @@
 import rclpy
 from rclpy.node import Node
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
+from rcl_interfaces.msg import ParameterDescriptor
 from tf2_ros import TransformBroadcaster
-from geometry_msgs.msg import TransformStamped
 from math import pi
 from .quaternion import angle_axis_to_quaternion
 
+from rclpy.callback_groups import ReentrantCallbackGroup
+
+from sensor_msgs.msg import JointState
+from nav_msgs.msg import Odometry
+from turtle_brick_interfaces.msg import Tilt
+from geometry_msgs.msg import TransformStamped, PoseStamped
+from turtlesim.msg import Pose
+
+class Position:
+    """ Class for storing the position of the turtle in turtlesim,
+        containing x, y, and theta
+    """
+    def __init__(self, x=0.0, y=0.0, theta=0.0):
+        self.x = x
+        self.y = y
+        self.theta = theta
 
 class TurtleRobot(Node):
+    """ Central interface between turtlesim and other ROS programs
     """
-    Moves some frames around.
-
-    Static Broadcasts:
-       world -> base
-    Broadcasts:
-       base -> left and base -> right
-    """
-
     def __init__(self):
+        # Initialize the node
         super().__init__('turtle_robot')
-        self.dx = 10  # used to control frame movement
-        # create the broadcaster
+        # Initialize variables
+        self.init_var()
+        # This node will use Reentrant Callback Groups for nested services
+        self.cbgroup = ReentrantCallbackGroup()
+
+        ###
+        ### Parameters
+        ###
+        # Declare and get the following parameters: platform_height, wheel_radius, max_velocity, gravity_accel
+        self.declare_parameter("platform_height", 2.5,
+                               ParameterDescriptor(description="The height between the turtle platform and the ground"))
+        self.platform_height = self.get_parameter("platform_height").get_parameter_value().double_value
+        self.declare_parameter("wheel_radius", 0.5,
+                               ParameterDescriptor(description="The proximity in which the turtle needs to be to a waypoint to classify as arrived"))
+        self.wheel_radius = self.get_parameter("wheel_radius").get_parameter_value().double_value
+        self.declare_parameter("max_velocity", 50.0,
+                               ParameterDescriptor(description="The velocity of the turtle"))
+        self.max_velocity = self.get_parameter("max_velocity").get_parameter_value().double_value
+        self.declare_parameter("gravity_accel", 9.81,
+                               ParameterDescriptor(description="The acceleration caused by gravity"))
+        self.gravity_accel = self.get_parameter("gravity_accel").get_parameter_value().double_value
+        # Declare and get extra parameters
+        self.declare_parameter("frequency", 100.0,
+                               ParameterDescriptor(description="The frequency in which the msg is published"))
+        self.frequency = self.get_parameter("frequency").get_parameter_value().double_value
+
+        ###
+        ### PUBLISHERS
+        ###
+        # Create publisher for joint_state
+        self.pub_jointstate = self.create_publisher(JointState, 'joint_states', 10)
+        # Create publisher for publishing odometry messages
+        self.pub_odom = self.create_publisher(Odometry, 'odom', 10)
+
+        ###
+        ### SUBSCRIBERS
+        ###
+        # Create subscriber for goal_pose messages
+        self.sub_goal = self.create_subscription(PoseStamped, 'goal_pose', self.sub_goal_callback, 10)
+        self.sub_goal # Used to prevent warnings
+        # Create subscriber for tilt messages
+        self.sub_tilt = self.create_subscription(Tilt, 'tilt', self.sub_tilt_callback, 10)
+        self.sub_tilt # Used to prevent warnings
+        # Create subscriber for getting the turtle's position in turtlesim
+        self.sub_pos = self.create_subscription(Pose, 'turtle1/pose', self.sub_pos_callback, 10)
+        self.sub_pos # Used to prevent warnings
+
+        ###
+        ### BROADCASTER
+        ###
+        # create the broadcaster for transforms
         self.broadcaster = TransformBroadcaster(self)
-        # Create a timer to do the rest of the transforms
-        self.tmr = self.create_timer(1, self.timer_callback)
+
+        ###
+        ### TIMER
+        ###
+        # Adjusted frequency for whatever the frequency param value is
+        timer_period = 1.0/self.frequency  # seconds
+        # create timer and timer callback
+        self.timer = self.create_timer(timer_period, self.timer_callback, callback_group=self.cbgroup)
+
+    def init_var(self):
+        """ Initialize all of the turtle_robot node's variables
+        """
+        # Position and TF
+        self.init_odom = False # whether odom Position has been initialized
+        self.odom = Position() # starting position/odom/spawn location
+        self.old_pos = Position()
+        self.new_pos = Position()
+        self.transform = Position()
 
     def timer_callback(self):
-        # Now create the transform, noted that it must have a parent frame and a timestamp
-        # The header contains the timing information and frame id
-        world_base_tf = TransformStamped()
-        world_base_tf.header.stamp = self.get_clock().now().to_msg()
-        world_base_tf.header.frame_id = "world"
-        world_base_tf.child_frame_id = "base_link"
-
-        # get a quaternion corresponding to a rotation by theta about an axis
-        degrees = 36 * self.dx
-        radians = degrees * pi / 180.0
-        
-        # The base frame will be raised in the z direction by 1 meter
-        # and be aligned with world We are relying on the default values
-        # of the transform message (which defaults to no rotation)
-        world_base_tf.transform.translation.z = 0.4
-
-        radians = degrees * pi / 180.0
-        world_base_tf.transform.rotation = angle_axis_to_quaternion(radians, [0, 0, 1.0])
-
-        self.broadcaster.sendTransform(world_base_tf)
-
-        base_left = TransformStamped()
-        base_left.header.frame_id = "base_link"
-        base_left.child_frame_id = "left"
-        base_left.transform.translation.x = -float(self.dx)
-        
-        base_left.transform.rotation = angle_axis_to_quaternion(radians, [0, 0, 1.0])
-
-        base_right = TransformStamped()
-        base_right.header.frame_id = "base_link"
-        base_right.child_frame_id = "right"
-        base_right.transform.translation.x = float(self.dx)
-        base_right.transform.rotation = angle_axis_to_quaternion(radians, [0, 0, -1.0])
-
-        # don't forget to put a timestamp
+        # Initialize the current time for all the tfs
         time = self.get_clock().now().to_msg()
-        base_right.header.stamp = time
-        base_left.header.stamp = time
+        
+        # Create the transform for world -> odom and odom -> base_link
+        world_odom_tf = TransformStamped()
+        world_odom_tf.header.stamp = time
+        world_odom_tf.header.frame_id = "world"
+        world_odom_tf.child_frame_id = "odom"
+        temp_tf = self.new_transform(Position(), self.odom)
+        world_odom_tf = self.update_tf(world_odom_tf, temp_tf)
+        self.broadcaster.sendTransform(world_odom_tf)
 
-        self.broadcaster.sendTransform(base_left)
-        self.broadcaster.sendTransform(base_right)
+        odom_base_tf = TransformStamped()
+        odom_base_tf.header.stamp = time
+        odom_base_tf.header.frame_id = "odom"
+        odom_base_tf.child_frame_id = "base_link"
+        temp_tf = self.new_transform(self.odom, self.new_pos)
+        odom_base_tf = self.update_tf(odom_base_tf, temp_tf)
+        self.broadcaster.sendTransform(odom_base_tf)
+    
+    ###
+    ### SUBSCRIBER CALLBACKS
+    ###
+    def sub_goal_callback(self, msg):
+        pass
 
-        # update the movement
-        self.dx -= 0.5
-        if self.dx == 0:
-            self.dx = 10
+    def sub_tilt_callback(self, msg):
+        pass
+
+    def sub_pos_callback(self, msg):
+        """ Callback function for the turtle1/pose topic.
+
+            Receives the turtlesim's current position and updates the turtle robot's position.
+            If the node has just been initialized (and the turtle has just spawned),
+            take note of the starting position.
+            
+            Args:
+                msg (turtlesim/Pose): A message that contains a Pose message, containing the
+                    current x, y, theta, and linear and angular velocities of the turtle
+        """
+        if not self.init_odom:
+            self.odom = Position(msg.x, msg.y, msg.theta)
+            self.old_pos = Position(msg.x, msg.y, msg.theta)
+            self.new_pos = Position(msg.x, msg.y, msg.theta)
+            self.init_odom = True
+        elif self.init_odom:
+            self.old_pos = self.new_pos
+            self.new_pos = Position(msg.x, msg.y, msg.theta)
+
+    ###
+    ### HELPER FUNCTIONS
+    ###
+    def new_transform(self, old, new):
+        """ Returns a Position that reflects the transform between old and new positions
+        """
+        tf = Position()
+        tf.x = old.x - new.x
+        tf.y = old.y - new.y
+        tf.theta = old.theta - new.theta
+        return tf
+
+    def update_tf(self, input_tf, change):
+        """ Returns an updated TransformStamped that reflects the transform/change in position
+        """
+        tf = input_tf
+        tf.transform.translation.x = change.x
+        tf.transform.translation.y = change.y
+        tf.transform.rotation = angle_axis_to_quaternion(change.theta, [0, 0, -1.0])
+        return tf
+
 
 
 def turtle_robot_entry(args=None):

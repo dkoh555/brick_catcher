6988c4167e9b079aaa3ebfa9fd5c0b5c290150d2
dkoh555 <damienkoh2025@u.northwestern.edu>
2023-10-12 22:29:32 -0500

    implemented yaml with xacro and turtle_robot node

--- turtle_brick/setup.py
+++ turtle_brick/setup.py
@@ -26,7 +26,8 @@ setup(
     entry_points={
         'console_scripts': [
             'test_node = turtle_brick.test_node:main',
-            'turtle_robot = turtle_brick.turtle_robot:turtle_robot_entry'
+            'turtle_robot = turtle_brick.turtle_robot:turtle_robot_entry',
+            'arena = turtle_brick.arena:arena_entry',
         ],
     },
 )

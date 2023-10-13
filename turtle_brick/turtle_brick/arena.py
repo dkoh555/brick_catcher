f6f3416fca91ddcb058b4612af4de7785acfed2b
dkoh555 <damienkoh2025@u.northwestern.edu>
2023-10-13 01:50:14 -0500

    added the brick, now need to program in its FALLING behaviour

--- turtle_brick/turtle_brick/arena.py
+++ turtle_brick/turtle_brick/arena.py
@@ -8,13 +8,35 @@ from rcl_interfaces.msg import ParameterDescriptor
 
 from rclpy.callback_groups import ReentrantCallbackGroup
 
-from geometry_msgs.msg import TransformStamped, Pose
+from geometry_msgs.msg import TransformStamped, Pose, Quaternion
 from std_srvs.srv import Empty
 from turtle_brick_interfaces.srv import Place
 
 from .quaternion import angle_axis_to_quaternion
 from rclpy.qos import QoSProfile, QoSDurabilityPolicy
 import math
+
+from enum import Enum
+
+class state(Enum):
+    """ Current state of the system (arena node).
+        Determines what movement commands are published to the turtle robot,
+        whether it is MOVING or STOPPED
+    """
+    FALLING = 0
+    STOPPED = 1
+    PLATFORM = 2
+
+class Position3D:
+    """ Class for storing the position of the brick in the arena,
+        containing x, y, z, and theta.
+        (Only one DoF for rotation because turtle_robot platform also only has one rotational DoF)
+    """
+    def __init__(self, x=0.0, y=0.0, z=0.0, theta=0.0):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.theta = theta
 
 class Arena(Node):
     """ Node for simulating the environment in rviz
@@ -64,18 +86,43 @@ class Arena(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def init_var(self):
-        # initialize variables
+        # initialize arena variables
         self.arena_length = 11.12
         self.wall_width = 0.005
         self.wall_length = self.arena_length + 2.0 * self.wall_width
         self.wall_height = 1.0
+        # initialize brick variables
+        self.brick_dim = [0.5, 0.3, 0.3] # x, y, z measurements
+        self.brick_pos = Position3D(3.0, 3.0, 7.5, 0.0)
+        # initialize general node variables
+        self.state = state.STOPPED
         self.frequency = 250
 
     ###
     ### TIMER CALLBACK
     ### 
     def timer_callback(self):
+        # Initialize the current time
+        time = self.get_clock().now().to_msg()
+
+        # Declaring all the import variables/transforms/joint states
+        world_brick_tf = TransformStamped()
+
+        # If the brick is in a FALLING state, adjust the tf
+        if self.state == state.FALLING:
+            pass
+
+        # Create the transform for world -> brick
+        world_brick_tf.header.stamp = time
+        world_brick_tf.header.frame_id = "world"
+        world_brick_tf.child_frame_id = "brick"
+        temp_tf = self.new_transform(self.brick_pos, Position3D())
+        world_brick_tf = self.update_tf(world_brick_tf, temp_tf)
+        self.broadcaster.sendTransform(world_brick_tf)
+
+        # Publishing Markers
         self.pub_walls()
+        self.pub_brick()
 
     ###
     ### SERVICE CALLBACKS
@@ -89,6 +136,53 @@ class Arena(Node):
     ###
     ### HELPER FUNCTIONS
     ###
+    def new_transform(self, new, old):
+        """ Returns a Position3D that reflects the transform between old and new positions
+        """
+        tf = Position3D()
+        tf.x = new.x - old.x
+        tf.y = new.y - old.y
+        tf.z = new.z - old.z
+        tf.theta = new.theta - old.theta
+        return tf
+    
+    def update_tf(self, input_tf, change):
+        """ Returns an updated TransformStamped that reflects the transform/change in position
+        """
+        tf = input_tf
+        tf.transform.translation.x = change.x
+        tf.transform.translation.y = change.y
+        tf.transform.translation.z = change.z
+        tf.transform.rotation = angle_axis_to_quaternion(change.theta, [0.0, 0.0, -1.0])
+        return tf
+
+    ###
+    ### MARKER FUNCTIONS
+    ###
+    def pub_brick(self):
+        self.brick = Marker()
+        self.brick.header.frame_id = "brick"
+        self.brick.header.stamp = self.get_clock().now().to_msg()
+        self.brick.id = 5
+        self.brick.type = Marker.CUBE
+        self.brick.action = Marker.ADD
+        self.brick.scale.x = self.brick_dim[0]
+        self.brick.scale.y = self.brick_dim[1]
+        self.brick.scale.z = self.brick_dim[2]
+        self.brick.pose.position.x = 0.0
+        self.brick.pose.position.y = 0.0
+        self.brick.pose.position.z = self.brick_dim[2]/2 # The brick frame is at the bottom face of the brick marker 
+        quaternion = angle_axis_to_quaternion(0.0, [0.0, 0.0, 1.0])
+        self.brick.pose.orientation.x = quaternion.x
+        self.brick.pose.orientation.y = quaternion.y
+        self.brick.pose.orientation.z = quaternion.z
+        self.brick.pose.orientation.w = quaternion.w
+        self.brick.color.r = 1.0
+        self.brick.color.g = 0.0
+        self.brick.color.b = 1.0
+        self.brick.color.a = 1.0
+        self.pub_wall.publish(self.brick)
+
     def pub_walls(self):
         self.n_wall = Marker()
         self.n_wall.header.frame_id = "world"
